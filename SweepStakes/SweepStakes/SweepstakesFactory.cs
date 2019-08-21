866ebe57eb92599d82d277029fd5e1cdb6a797be
Nathaniel Kinderman <natekinderman16@gmail.com>
2019-08-21 14:58:25 -0500

    Created SweepstakesFactory

--- SweepStakes/SweepStakes/SweepstakesFactory.cs
+++ SweepStakes/SweepStakes/SweepstakesFactory.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace SweepStakes
 {
-    class SweepstakesFactory
+    class SweepstakesFactory 
     {
+    
+        //method injection but how????
+    //make a method and then use iSweepstakesMananger and then a swtich case for user to choose queue or stack
+    public ISweepstakesManager ChooseYourSweepstakesManagerStyle()
+        {
+            Console.WriteLine("");
+               string thing = Console.ReadLine().ToLower();
+            switch (thing))
+            {
+                case "stack":
+                    return new SweepstakesStackManager();
+
+                case "queue":
+                    return new SweepstakesQueueManager();
+
+
+            }
+
+        }
+
     }
 }
